49a8c57548e2b6e0f668a53e14071fac37a248b5
Arpit Babbar <arpitbabbar@gmail.com>
2026-01-30 09:06:54 +0100

    Running but crashing version

--- Examples/examples_crk/2d/run_multiion_convergence.jl
+++ Examples/examples_crk/2d/run_multiion_convergence.jl
@@ -133,16 +133,16 @@ end
 initial_value = (x, y) -> exact_solution_manufactured_solution(x, y, 0.0)
 
 degree = 1
-solver = cRK22()
-solution_points = "gl"
-correction_function = "radau"
+solver = TrixiRKSolver()
+solution_points = "gll"
+correction_function = "g2"
 numerical_flux = Eq.rusanov
 bound_limit = "no"
 bflux = evaluate
 final_time = 1.0
 
-nx = 100
-ny = 100
+nx = 8
+ny = 8
 cfl = 0.0
 bounds = ([-Inf], [Inf]) # Not used in Euler
 tvbM = 0.0
