44cd980940712939fc2217ac36a491dd0af36efa
Arpit Babbar <arpitbabbar@gmail.com>
2026-01-31 21:20:53 +0100

    Working KHI with cRK VolumeIntegralFluxDifferencing

--- Examples/examples_crk/2d/run_multiion_khi_crk_es.jl
+++ Examples/examples_crk/2d/run_multiion_khi_crk_es.jl
@@ -9,11 +9,11 @@ import Tenkai.TenkaicRK.Trixi
 ###############################################################################
 
 # semidiscretization of the ideal multi-ion MHD equations
+cfl_safety_factor = 0.34
 trixi_equations = IdealGlmMhdMultiIonEquations2D(gammas = (5 / 3, 1.4),
                                                  charge_to_mass = (1, 1 / 2),
                                                  initial_c_h = 0.0)
-
-eq = Eq.get_equation(trixi_equations) # Deactivate GLM divergence cleaning
+eq = Eq.get_equation(trixi_equations; glm_scale = cfl_safety_factor) # Deactivate GLM divergence cleaning
 
 """
 Initial (and exact) solution for the the manufactured solution test. Runs with 
@@ -79,16 +79,15 @@ bound_limit = "no"
 bflux = evaluate
 final_time = 20.0
 
-nx = 32
-ny = 32
+nx = 64
+ny = 64
 cfl = 0.0
 bounds = ([-Inf], [Inf]) # Not used in Euler
 tvbM = 0.0
 save_iter_interval = 0
-save_time_interval = 0.0
+save_time_interval = 0.5
 animate = true # Factor on save_iter_interval or save_time_interval
 compute_error_interval = 0
-cfl_safety_factor = 0.4
 
 xmin, xmax = -1.0, 1.0
 ymin, ymax = -1.0, 1.0
@@ -108,15 +107,6 @@ grid_size = [nx, ny]
 
 problem = Problem(domain, initial_value, boundary_value, boundary_condition,
                   final_time, exact_solution_khi, source_terms = source_terms_lorentz_khi)
-limiter_blend = setup_limiter_blend(blend_type = fo_blend(eq),
-                                    # indicating_variables = Eq.rho_p_indicator!,
-                                    indicating_variables = conservative_indicator!,
-                                    reconstruction_variables = conservative_reconstruction,
-                                    indicator_model = "gassner",
-                                    debug_blend = false,
-                                    amax = 0.01,
-                                    pure_fv = false)
-limiter = limiter_blend
 limiter = setup_limiter_none()
 scheme = Scheme(solver, degree, solution_points, correction_function,
                 numerical_flux, bound_limit, limiter, bflux)
